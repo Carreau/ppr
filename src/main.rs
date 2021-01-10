b8761659bb014aada3234132281a42f539dbb9ae
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-01-10 11:36:43 -0800

    condense

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use glob::glob;
+use indicatif::{ProgressBar, ProgressIterator};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use serde_tuple::{Deserialize_tuple, Serialize_tuple};
@@ -8,23 +9,6 @@ use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct A {
-    value: i32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct B {
-    value: i32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", content = "c")]
-enum AorB {
-    X(A),
-    Y(B),
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Section {
@@ -86,6 +70,7 @@ struct DefListItem {
 struct CodeEntry {
     token: String,
     target: Option<String>,
+    pygc: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -121,41 +106,31 @@ fn read_data_from_file<P: AsRef<Path>>(path: P) -> Result<Document, Box<dyn Erro
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    //let sec = Section {
-    //    children: Some(vec!["a".to_owned(), "b".to_owned()]),
-    //    tag: AorB::X(A { value: 1 }),
-    //};
-    //println!("Hello, world! {:?}", &sec);
+    let pth: Vec<_> = glob("/Users/bussonniermatthias/.papyri/ingest/*/*/module/*.json")?.collect();
 
-    //let data = serde_json::to_string(&sec).unwrap();
-    //println!("Hello, world! {}", data);
-
-    for mp in glob("/Users/bussonniermatthias/.papyri/ingest/*/*/module/*.json").unwrap()
-    //.take(500)
-    {
+    for mp in pth.iter().progress() {
         if let Ok(p) = mp {
-            println!("{:?}", p.display());
+            //println!("{:?}", p.display());
             let document = read_data_from_file(p)?;
-            if let Some(example) = document.example_section_data {
-                if let Some(ee) = example.children {
-                    for c in ee {
-                        match c {
-                            TopLevelBlock::Paragraph(_) => (),
-                            TopLevelBlock::DefList(_) => (),
-                            TopLevelBlock::BlockDirective(_) => (),
-                            TopLevelBlock::Fig(_) => (),
-                            TopLevelBlock::Code(code) => {
-                                code.entries
-                                    .into_iter()
-                                    .for_each(|entry| match entry.target {
-                                        Some(e) => {
-                                            if e.is_empty() == false {
-                                                println!("  {}", e)
-                                            }
+            if let Some(Some(ee)) = document.example_section_data {
+                for c in ee {
+                    match c {
+                        TopLevelBlock::Paragraph(_) => (),
+                        TopLevelBlock::DefList(_) => (),
+                        TopLevelBlock::BlockDirective(_) => (),
+                        TopLevelBlock::Fig(_) => (),
+                        TopLevelBlock::Code(code) => {
+                            code.entries
+                                .into_iter()
+                                .for_each(|entry| match entry.target {
+                                    Some(e) => {
+                                        if e.is_empty() == false {
+                                            //&prog.println(format!("  {}", &e));
+                                            ()
                                         }
-                                        None => (),
-                                    })
-                            }
+                                    }
+                                    None => (),
+                                })
                         }
                     }
                 }
