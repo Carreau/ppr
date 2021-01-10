2007257dde462693f83a4ccef9f7c218d11a3e69
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-01-10 10:10:20 -0800

    deepeer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use glob::glob;
 use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value};
+use serde_json::Value;
 use serde_tuple::{Deserialize_tuple, Serialize_tuple};
 use std::collections::HashMap;
 
@@ -50,12 +50,12 @@ struct Document {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ExampleSectionData {
-    children: Option<Vec<TLB>>,
+    children: Option<Vec<TopLevelBlock>>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type", content = "data")]
-enum TLB {
+enum TopLevelBlock {
     Paragraph(Paragraph),
     DefList(DefList),
     Code(Code),
@@ -93,14 +93,16 @@ struct Code {
     out: String,
 }
 
-fn read_data_from_file<P: AsRef<Path>>(path: P) -> Document {
-    let file = File::open(path).unwrap();
+fn read_data_from_file<P: AsRef<Path>>(path: P) -> Result<Document, Box<dyn Error>> {
+    let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    serde_json::from_reader(reader).unwrap()
+    let u = serde_json::from_reader(reader)?;
+
+    Ok(u)
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     //let sec = Section {
     //    children: Some(vec!["a".to_owned(), "b".to_owned()]),
     //    tag: AorB::X(A { value: 1 }),
@@ -110,23 +112,29 @@ fn main() {
     //let data = serde_json::to_string(&sec).unwrap();
     //println!("Hello, world! {}", data);
 
-    for mp in glob("/Users/bussonniermatthias/.papyri/ingest/*/*/module/*.json")
-        .unwrap()
-        .take(500)
+    for mp in glob("/Users/bussonniermatthias/.papyri/ingest/*/*/module/*.json").unwrap()
+    //.take(500)
     {
         if let Ok(p) = mp {
             //println!("{:?}", p.display());
-            let document = read_data_from_file(p);
+            let document = read_data_from_file(p)?;
             if let Some(example) = document.example_section_data {
                 if let Some(ee) = example.children {
                     for c in ee {
                         match c {
-                            TLB::Paragraph(_) => println!("{:?}", "Paragraph"),
-                            TLB::DefList(_) => println!("{:?}", "Deflist"),
-                            TLB::Code(code) => {
-                                println!("{:?}", code.ce_status);
-                                println!("    {:?}", code.entries);
-                                println!("    {:?}", code.out);
+                            TopLevelBlock::Paragraph(_) => (),
+                            TopLevelBlock::DefList(_) => (),
+                            TopLevelBlock::Code(code) => {
+                                code.entries
+                                    .into_iter()
+                                    .for_each(|entry| match entry.target {
+                                        Some(e) => {
+                                            if e.is_empty() == false {
+                                                println!("{}", e)
+                                            }
+                                        }
+                                        None => (),
+                                    })
                             }
                         }
                     }
@@ -134,4 +142,5 @@ fn main() {
             }
         }
     }
+    Ok(())
 }
